2044e86e32583ce2e5c75bb01910a491cc3ac1b7
ghbecerra <ghbecerra@hotmail.com>
2016-12-06 19:53:11 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -17,6 +17,8 @@ function Mostrar()
 		if (numeroIngresado == 0) {
 			contadorceros++;
 			} else {
+				if ((numeroIngresado%2)==0)
+					contadorpares++;
 				if (numeroIngresado > 0) {
 					contadorpositivos++;
 					positivo=positivo+numeroIngresado;
@@ -32,7 +34,7 @@ function Mostrar()
 	document.write("<br>Suma de los positivos: "+positivo);
 	document.write("<br>Cantidad de positivos: "+contadorpositivos);
 	document.write("<br>Cantidad de negativos: "+contadornegativos);
-	document.write("<br>Cantidad de números pares: ");
+	document.write("<br>Cantidad de números pares: "+contadorpares);
 	document.write("<br>Promedio de positivos: "+positivo/contadorpositivos);
 	document.write("<br>Promedios de negativos: "+negativo/contadornegativos);
 	document.write("<br>Diferencia entre positivos y negativos: "+(positivo-negativo));
